bff491044fbae2a57c1018c2f73934fa5c0db0a1
123 <myEmail@example.com>
2023-06-18 16:52:53 +0500

    upload a new files

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # telegram api
-token = '6042653605:AAHIztBW2H1EPLSTThAQyQgU4aWWKsA52kA'
+token = 'YOUR_DATA'
 
 # connect to database
 host = 'localhost'
 user = 'postgres'
-password = 'root'
-db_name = 'vlada'
+password = 'YOUR_DATA'
+db_name = 'YOUR_DATA'
 port = '5432'
